09fa6bf93463d6ebe2c808817c481e65fc71e5ce
ScipioAemilianus <m@m.m0k.eu>
2014-09-30 00:25:24 +0200

    Fixed static Data endpoint

--- LolApi/src/main/java/eu/m0k/lol/api/services/StaticDataService.java
+++ LolApi/src/main/java/eu/m0k/lol/api/services/StaticDataService.java
@@ -2,7 +2,6 @@ package eu.m0k.lol.api.services;
 
 import eu.m0k.lol.api.model.ChampData;
 import eu.m0k.lol.api.model.ChampionList;
-import eu.m0k.lol.api.model.Match;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import retrofit.http.Query;
@@ -12,26 +11,26 @@ import retrofit.http.Query;
  */
 public interface StaticDataService {
     @GET("/v1.2/champion")
-    public ChampionList getScores();
+    public ChampionList getChampionList();
 
     @GET("/v1.2/champion")
-    public ChampionList getScores(@Query("locale") String locale);
+    public ChampionList getChampionList(@Query("locale") String locale);
 
     @GET("/v1.2/champion")
-    public ChampionList getScores(@Query("locale") String locale, @Query("champData") ChampData champData);
+    public ChampionList getChampionList(@Query("locale") String locale, @Query("champData") ChampData champData);
 
     @GET("/v1.2/champion")
-    public String getScores(@Query("champData") ChampData champData);
+    public ChampionList getChampionList(@Query("champData") ChampData champData);
 
     @GET("/v1.2/champion")
-    public String getScores(@Query("dataById") boolean databyId, @Query("champData") ChampData champData);
+    public ChampionList getChampionList(@Query("dataById") boolean databyId, @Query("champData") ChampData champData);
 
     @GET("/v1.2/champion")
-    public String getScores(@Query("locale") String locale, @Query("dataById") boolean databyId, @Query("champData") ChampData champData);
+    public ChampionList getChampionList(@Query("locale") String locale, @Query("dataById") boolean databyId, @Query("champData") ChampData champData);
 
     @GET("/v1.2/champion")
-    public String getScores(@Query("locale") String locale, @Query("version") String version, @Query("dataById") boolean databyId, @Query("champData") ChampData champData);
+    public ChampionList getChampionList(@Query("locale") String locale, @Query("version") String version, @Query("dataById") boolean databyId, @Query("champData") ChampData champData);
 
     @GET("/v1.2/champion/{id}")
-    public Match getScores(@Path("id") int id, @Query("locale") String locale, @Query("version") String version, @Query("champData") ChampData champData);
+    public ChampionList getChampionList(@Path("id") int id, @Query("locale") String locale, @Query("version") String version, @Query("champData") ChampData champData);
 }
