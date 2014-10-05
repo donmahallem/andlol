965b6fab152eb2a88a78a58b8816195487dc6b12
ScipioAemilianus <m@m.m0k.eu>
2014-10-05 15:32:34 +0200

    Added ProgressDialog to ApiToken Fragment

--- app/src/main/java/eu/mok/mokeulol/fragments/ApiTokenFragment.java
+++ app/src/main/java/eu/mok/mokeulol/fragments/ApiTokenFragment.java
@@ -8,10 +8,14 @@
 
 package eu.mok.mokeulol.fragments;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import eu.mok.mokeulol.R;
@@ -19,8 +23,11 @@ import eu.mok.mokeulol.R;
 /**
  * Created by Don on 05.10.2014.
  */
-public class ApiTokenFragment extends LeagueFragment {
+public class ApiTokenFragment extends LeagueFragment implements View.OnClickListener, DialogInterface.OnCancelListener {
     private EditText mTxtApiToken;
+    private Button mBtnCheckToken;
+    private ProgressDialog mProgressDialog;
+    private Task mTask;
 
     public ApiTokenFragment() {
         super();
@@ -39,5 +46,69 @@ public class ApiTokenFragment extends LeagueFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         this.mTxtApiToken = (EditText) view.findViewById(R.id.apiToken);
+        this.mBtnCheckToken = (Button) view.findViewById(R.id.btnCheckToken);
+        // Adding Listeners
+        this.mBtnCheckToken.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == this.mBtnCheckToken) {
+            this.mTask = new Task();
+            this.mProgressDialog = new ProgressDialog(this.getActivity());
+            this.mProgressDialog.setTitle(R.string.checking);
+            this.mProgressDialog.setOnCancelListener(this);
+            this.mProgressDialog.show();
+            this.mTask.execute("token");
+        }
+    }
+
+    @Override
+    public void onCancel(DialogInterface dialog) {
+        if (this.mTask != null && this.mTask.getStatus() != AsyncTask.Status.FINISHED) {
+            this.mTask.cancel(true);
+        }
+    }
+
+    private class Task extends AsyncTask<String, Integer, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            this.publishProgress(0);
+            if (isCancelled())
+                return false;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+            this.publishProgress(1);
+            if (isCancelled())
+                return false;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+            this.publishProgress(2);
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            ApiTokenFragment.this.mProgressDialog.hide();
+            ApiTokenFragment.this.mProgressDialog = null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            if (values[0] == 0)
+                ApiTokenFragment.this.mProgressDialog
+                        .setMessage(ApiTokenFragment.this.getActivity().getResources().getString(R.string.starting));
+            if (values[0] == 1)
+                ApiTokenFragment.this.mProgressDialog
+                        .setMessage(ApiTokenFragment.this.getActivity().getResources().getString(R.string.checking_token));
+            if (values[0] == 2)
+                ApiTokenFragment.this.mProgressDialog
+                        .setMessage(ApiTokenFragment.this.getActivity().getResources().getString(R.string.checked));
+        }
     }
 }
