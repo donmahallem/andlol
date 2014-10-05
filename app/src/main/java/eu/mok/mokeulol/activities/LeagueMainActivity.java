60f353ccb2059230800f3f6b301def76540a7193
ScipioAemilianus <m@m.m0k.eu>
2014-10-05 14:55:02 +0200

    Reworked the Api Token Fragment

--- app/src/main/java/eu/mok/mokeulol/activities/LeagueMainActivity.java
+++ app/src/main/java/eu/mok/mokeulol/activities/LeagueMainActivity.java
@@ -12,8 +12,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import eu.mok.mokeulol.R;
 import eu.mok.mokeulol.fragments.ApiTokenFragment;
@@ -26,30 +24,11 @@ public class LeagueMainActivity extends FragmentActivity implements LeagueFragme
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_main);
         Fragment fragment = this.getSupportFragmentManager().findFragmentById(android.R.id.content);
         if (fragment == null) {
             onShowApiTokenFragment();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
