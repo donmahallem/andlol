747e6c9b0ea117f808a9ecd9fe9879ba350f6af4
ScipioAemilianus <m@m.m0k.eu>
2014-10-17 18:47:59 +0200

    Added Module for custom Views

---
+++ MokeuViews/src/androidTest/java/eu/m0k/lol/views/ApplicationTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2014.
+ *
+ * Visit https://github.com/donmahallem/andlol for more info!
+ *
+ * Romanes eunt domus - Brian!
+ */
+
+package eu.m0k.lol.views;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
