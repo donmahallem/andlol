deb47fdf995f5a2beef69f3c91d37856e8c8bfe6
ScipioAemilianus <m@m.m0k.eu>
2014-10-18 06:13:28 +0200

    Rotating Button Stuff

--- LeagueViews/src/main/java/eu/m0k/lol/views/FavoriteButton.java
+++ LeagueViews/src/main/java/eu/m0k/lol/views/FavoriteButton.java
@@ -11,28 +11,45 @@ package eu.m0k.lol.views;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RotateDrawable;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageButton;
 
 import eu.m0k.lol.views.drawable.FavoriteDrawable;
 
 public class FavoriteButton extends ImageButton {
-    private FavoriteDrawable mFavoriteDrawable = new FavoriteDrawable();
+    private Drawable mDrawable;
+    private FavoriteDrawable aaaa;
 
+    private RotateDrawable RotateIcon = new RotateDrawable() {
+    };
     public FavoriteButton(Context context) {
         super(context);
-        super.setImageDrawable(this.mFavoriteDrawable);
+        setup(context);
     }
 
     public FavoriteButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        super.setImageDrawable(this.mFavoriteDrawable);
+        setup(context);
     }
 
     public FavoriteButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        super.setImageDrawable(this.mFavoriteDrawable);
+        setup(context);
+    }
+
+    private void setup(Context context) {
+        aaaa = new FavoriteDrawable(context.getResources().getDrawable(R.drawable.ic_favorite));
+        super.setImageDrawable(aaaa);
+        //super.setImageResource(R.drawable.btn_favorite_drawable);
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aaaa.addRotation(10);
+            }
+        });
     }
 
     @Override
--- LeagueViews/src/main/java/eu/m0k/lol/views/drawable/FavoriteDrawable.java
+++ LeagueViews/src/main/java/eu/m0k/lol/views/drawable/FavoriteDrawable.java
@@ -8,18 +8,37 @@
 
 package eu.m0k.lol.views.drawable;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
 public class FavoriteDrawable extends Drawable {
     private final Paint mPaint = new Paint();
+    private final Bitmap mBitmap;
+    private float mRotation = 0f;
 
-    public FavoriteDrawable() {
-        this.mPaint.setARGB(128, 255, 0, 0);
+    public FavoriteDrawable(Drawable drawable) {
+        this.mBitmap = drawableToBitmap(drawable);
+        this.mPaint.setARGB(255, 255, 0, 0);
         this.mPaint.setStrokeWidth(0.1f);
+    }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
     }
 
     @Override
@@ -27,14 +46,28 @@ public class FavoriteDrawable extends Drawable {
         final int size = Math.round(Math.min(canvas.getWidth(), canvas.getHeight()) * 0.8f);
         final int marginTop = canvas.getHeight() - size;
         final int marginLeft = canvas.getWidth() - size;
-        canvas.scale(canvas.getWidth(), canvas.getHeight());
-        canvas.drawLine(0.1f, 0.1f, 0.8f, 0.8f, this.mPaint);
+        Rect src = new Rect();
+        Rect dst = new Rect();
+        src.set(0, 0, this.mBitmap.getWidth(), this.mBitmap.getHeight());
+        dst.set(0, 0, canvas.getWidth(), canvas.getHeight());
+        canvas.rotate(mRotation, canvas.getWidth() / 2f, canvas.getHeight() / 2f);
+        canvas.drawBitmap(this.mBitmap, src, dst, mPaint);
+    }
+
+    public void setRotation(float rotation) {
+        this.mRotation = rotation % 360;
+        invalidateSelf();
+    }
+
+    public void addRotation(float rotation) {
+        this.setRotation(this.mRotation + rotation);
     }
 
     @Override
     public void setAlpha(int alpha) {
         this.mPaint.setAlpha(alpha);
         this.invalidateSelf();
+        ColorFilter fi = new ColorFilter();
     }
 
     @Override
